c59d5511f0bd592192fa7bc487dd601e133c830e
rmkessler84@gmail.com <rmkessler84@gmail.com>
2019-01-21 10:13:47 -0500

    formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,37 +7,37 @@ import { WordService } from '../app/word.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-constructor(private wordService: WordService){
-  this.generateEncounter();
-}
+  constructor(private wordService: WordService) {
+    this.generateEncounter();
+  }
 
   title = 'encounter-generator';
   encounter: string;
 
-   generateEncounter(): void {
+  generateEncounter(): void {
     this.encounter = `The party must ${this.wordService.getVerb()} ${this.determineAorAn(this.ojectOrCreature())} in 
     the ${this.wordService.getObjectAdjective()} ${this.wordService.getLocation()}
     but they are ${this.wordService.getInterruption()} ${this.determineAorAn(this.wordService.getAdjective())}
      ${this.getCreature()}.`;
-   }
+  }
 
-   getCreature(): string {
-     let str = this.wordService.getCreature();
-    return Math.random() > .80 ? `${str} ${this.wordService.getClass()}` : str;
-   }
-   ojectOrCreature(): string {
-     return Math.random() % 2 == 0 ? 
-     `${this.wordService.getObjectAdjective()} ${this.wordService.getObject()}` : 
-     `${this.wordService.getAdjective()} ${this.wordService.getCreature()}`;
-   }
+  getCreature(): string {
+    let str = this.wordService.getCreature();
+    return Math.random() > .90 ? `${str} ${this.wordService.getClass()}` : str;
+  }
+  ojectOrCreature(): string {
+    return Math.random() % 2 == 0 ?
+      `${this.wordService.getObjectAdjective()} ${this.wordService.getObject()}` :
+      `${this.wordService.getAdjective()} ${this.wordService.getCreature()}`;
+  }
 
-   startsWithVowel(str: string): boolean {
+  startsWithVowel(str: string): boolean {
     let firstChar = str.toLowerCase().charAt(0);
-    let vowels = ['a','e','i','o','u'];
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
     return vowels.includes(firstChar);
-   }
+  }
 
-   determineAorAn(str: string): string {
-     return this.startsWithVowel(str) ? `an ${str}` : `a ${str}`;
-   }
+  determineAorAn(str: string): string {
+    return this.startsWithVowel(str) ? `an ${str}` : `a ${str}`;
+  }
 }
